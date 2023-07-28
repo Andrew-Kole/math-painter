c66a13b7960a067af3552125d4e8020ed0650071
pakitowka <whitesaint12@gmail.com>
2023-07-28 02:16:44 +0200

    implementing classes

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import numpy as np
+from PIL import Image
+
 
 class Canvas:
     """
@@ -8,8 +11,15 @@ class Canvas:
         self.height = height
         self.color = color
 
+        # Creating a numpy array of zeros
+        self.data = np.zeros((self.width, self.height, 3), dtype=np.uint8)
+        # Painting canvas in given color
+        self.data[:] = self.color
+
     def make(self, imagepath):
-        """image creating"""
+        """Converting a numpy array in image and saving it."""
+        img = Image.fromarray(self.data, 'RGB')
+        img.save(imagepath)
         pass
 
 
@@ -26,7 +36,7 @@ class Square:
 
     def draw(self, canvas):
         """Drawing figure"""
-        pass
+        canvas.data[self.x : self.x + self.side, self.y : self.y + self.side] = self.color
 
 
 class Rectangle:
@@ -43,4 +53,19 @@ class Rectangle:
 
     def draw(self, canvas):
         """drawing figure"""
-        pass
+        canvas.data[self.x : self.x + self.height, self.y : self.y + self.width] = self.color
+
+
+# Calling methods
+# Creating canvas for drawing
+canvas = Canvas(width=400, height=500, color=(255, 0, 0))
+# Creating rectangle
+rectangle = Rectangle(x=10, y=10, width=200, height=80, color=(0, 255, 0))
+# Drawing rectangle on canvas
+rectangle.draw(canvas)
+# Creating square
+square = Square(x=90, y=10, side=40, color=(0, 0, 255))
+# Drawing square
+square.draw(canvas)
+# creating a .png file
+canvas.make("output/canvas.png")
